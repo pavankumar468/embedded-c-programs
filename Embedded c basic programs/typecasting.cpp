93ee1812e955ab40eea4e5b0c0cb15d834a9d8d8
B Pavan kumar <62787933+pavankumar468@users.noreply.github.com>
2022-02-08 12:59:10 +0530

    Add files via upload

---
+++ Embedded c basic programs/typecasting.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	//implicit typecasting : it happened automatically by compiler, if there 2 variable of different datatype ,lower size datatype variable
+						//	can be implicitly copied to higher size datatype variable.
+	short int a=5;
+	char ch = 'A';
+	
+	float z= a+ch;
+	printf("%f",z);
+	
+	//Explicit typecasting :unlike implicit type-casting data-types of variable is forcefully converted into another datatype.
+					//->it is used when we move high size datatype to low size
+	int c=4, d=7,result;
+	result=d/c;
+	printf("\nint_result = %d",result); //here we are losing correct value
+	float float_result = (float)d/c;
+	printf("\n float_result = %f",float_result);
+}
